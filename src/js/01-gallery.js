28995fbf360e84cd3d1bca64a630818cd579664b
Roman <romeenice@gmail.com>
2022-12-28 20:10:25 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,13 +1,13 @@
 // Add imports above this line
 
-import { galleryItems } from "./gallery-items";
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 // Change code below this line
 
 const refs = {
-  imageContainer: document.querySelector(".gallery"),
+  imageContainer: document.querySelector('.gallery'),
   body: document.body,
 };
 
@@ -25,66 +25,64 @@ function newgalleryItems(items) {
     </a>
   </div>`;
     })
-    .join("");
+    .join('');
 }
 
 const cardgalleryMarkup = newgalleryItems(galleryItems);
-refs.imageContainer.insertAdjacentHTML("beforeend", cardgalleryMarkup);
+refs.imageContainer.insertAdjacentHTML('beforeend', cardgalleryMarkup);
 
-const lightbox = new SimpleLightbox(".gallery a");
+const lightbox = new SimpleLightbox('.gallery a');
 
-const createModalWindow = (imageAdress) => {
+const createModalWindow = imageAdress => {
   window.instance = lightbox.create(
     `
       <img src="${imageAdress}">
   `,
     {
       onShow: () =>
-        window.addEventListener("keydown", closeModalWindowByEscPressing),
+        window.addEventListener('keydown', closeModalWindowByEscPressing),
       onClose: () => {
-        window.removeEventListener("keydown", closeModalWindowByEscPressing);
-        refs.body.classList.remove("disable-scroll");
+        window.removeEventListener('keydown', closeModalWindowByEscPressing);
+        refs.body.classList.remove('disable-scroll');
       },
     }
   );
   return instance;
 };
 
-refs.imageContainer.addEventListener("click", onClickOpenModal);
+refs.imageContainer.addEventListener('click', onClickOpenModal);
 
 function onClickOpenModal(event) {
   event.preventDefault();
-  if (!event.target.classList.contains("gallery__image")) {
+  if (!event.target.classList.contains('gallery__image')) {
     return;
   }
   const originalImageRef = event.target.dataset.source;
   createModalWindow(originalImageRef).show();
-  refs.body.classList.add("disable-scroll");
+  refs.body.classList.add('disable-scroll');
 }
 
 function closeModalWindowByEscPressing(event) {
-  const ESC_KEY_CODE = "Escape";
+  const ESC_KEY_CODE = 'Escape';
   if (event.code === ESC_KEY_CODE && instance.visible()) {
     instance.close();
-    refs.body.classList.remove("disable-scroll");
+    refs.body.classList.remove('disable-scroll');
   }
 }
 
-const lazyImages = refs.imageContainer.querySelectorAll(".gallery__image");
+const lazyImages = refs.imageContainer.querySelectorAll('.gallery__image');
 
-lazyImages.forEach((image) =>
-  image.addEventListener("load", onImageLoaded, { once: true })
+lazyImages.forEach(image =>
+  image.addEventListener('load', onImageLoaded, { once: true })
 );
 
 function onImageLoaded(event) {
-  event.target.classList.add("appear");
+  event.target.classList.add('appear');
 }
 
-lazyImages.forEach((image) =>
-  image.addEventListener("mouseenter", onMouseEnter)
-);
+lazyImages.forEach(image => image.addEventListener('mouseenter', onMouseEnter));
 
 function onMouseEnter(event) {
-  event.target.style.transitionDelay = "100ms";
-  event.target.style.transitionDuration = "500ms";
+  event.target.style.transitionDelay = '100ms';
+  event.target.style.transitionDuration = '500ms';
 }
