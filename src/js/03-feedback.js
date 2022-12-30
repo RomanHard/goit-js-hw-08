f177f10fe2c2c94aaa6731f349730d5fd6d71d57
Roman <romeenice@gmail.com>
2022-12-30 21:21:26 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,5 +1,3 @@
-import throttle from 'lodash/throttle';
-
 const refs = {
   form: document.querySelector('.feedback-form'),
   textarea: document.querySelector('.feedback-form textarea'),
@@ -8,11 +6,24 @@ const refs = {
 refs.form.addEventListener('submit', onFormSubmit);
 refs.textarea.addEventListener('input', onTextareaInput);
 
-function onFormSubmit(evt) {}
+populateTextarea();
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  console.log('Отправляємо форму');
+
+  evt.currentTarget.reset();
+}
 
 function onTextareaInput(evt) {
   const message = evt.currentTarget.value;
-  console.log(value);
+  localStorage.setItem('feedback-form-state', message);
 }
 
-function populateNessageOutput() {}
+function populateTextarea() {
+  const savedMessage = localStorage.getItem('feedback-form-state');
+  if (savedMessage) {
+    refs.textarea.value = savedMessage;
+  }
+}
