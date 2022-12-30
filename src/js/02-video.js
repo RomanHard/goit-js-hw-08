4a5991d98952b621c46101800564de13750f70ce
Roman <romeenice@gmail.com>
2022-12-30 22:03:39 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,27 +1,32 @@
 import Player from '@vimeo/player';
-import throttle from 'lodash.throttle';
-
+const throttle = require('lodash.throttle');
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-
 player.on('play', function () {
   console.log('played the video!');
 });
-
-player.getVideoTitle().then(function (data) {
-  localStorage.setItem(
-    'videoplayer-current-time',
-    JSON.stringify(data.seconds)
-  );
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
 });
-
-player.on('timeupdate', throttle(getCurrentPlayTime, 1000));
-
-const getSecFromStorage = () => {
-  return JSON.parse(localStorage.getItem('videoplayer-current-time'));
-};
-if (localStorage.getItem('videoplayer-current-time')) {
-  player.setCurrentTime(getSecFromStorage()).then(function (seconds) {
-    console.log(`Last time you stopped video on ${second} seconds`);
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    console.log(data.seconds);
+    localStorage.setItem('timeUp', data.seconds);
+  }, 1000)
+);
+player
+  .setCurrentTime(localStorage.getItem('timeUp'))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+      default:
+        // some other error occurred
+        break;
+    }
   });
-}
