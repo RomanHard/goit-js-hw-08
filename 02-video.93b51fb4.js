88d85e54deb906db889093fdadb9402691a0a3a2
RomanHard <86237018+RomanHard@users.noreply.github.com>
2022-12-30 17:46:29 +0000

    Deploying to gh-pages from @ RomanHard/goit-js-hw-08@d7c3712edc07f43c46572a60c03922aaf2df3381 🚀

---
+++ 02-video.93b51fb4.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("iframe"),n=new Vimeo.Player(e);n.on("play",(function(){console.log("played the video!")})),n.getVideoTitle().then((function(e){console.log("title:",e)}));n.on("play",(function(e){})),n.on("eventName",(function(e){}))}();
+//# sourceMappingURL=02-video.93b51fb4.js.map
