bdeb209a2fe47e48ff6a06fa647005a02fe94212
Roman <romeenice@gmail.com>
2022-12-30 20:58:02 +0200

    feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,18 @@
+import throttle from 'lodash/throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('.feedback-form textarea'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', onTextareaInput);
+
+function onFormSubmit(evt) {}
+
+function onTextareaInput(evt) {
+  const message = evt.currentTarget.value;
+  console.log(value);
+}
+
+function populateNessageOutput() {}
