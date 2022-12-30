33b6882da40f793c9d4ce1d3206be31005a58355
Roman <romeenice@gmail.com>
2022-12-30 19:59:12 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,5 +1,5 @@
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
 player.on('play', function () {
   console.log('played the video!');
