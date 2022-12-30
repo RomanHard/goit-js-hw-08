d0972e53d46d647ddb3f20e71218c4530566c3f1
RomanHard <86237018+RomanHard@users.noreply.github.com>
2022-12-30 18:03:05 +0000

    Deploying to gh-pages from @ RomanHard/goit-js-hw-08@14be611bb1e9ff4ea88815a98d6e78cd39a5800b 🚀

--- 02-video.4c5d09f3.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("iframe"),t=new Player(e);t.on("play",(function(){console.log("played the video!")})),t.getVideoTitle().then((function(e){localStorage.setItem("videoplayer-current-time",JSON.stringify(e.seconds))})),t.on("timeupdate",throttle(getCurrentPlayTime,1e3));localStorage.getItem("videoplayer-current-time")&&t.setCurrentTime(JSON.parse(localStorage.getItem("videoplayer-current-time"))).then((function(e){console.log("Last time you stopped video on ".concat(second," seconds"))}))}();
-//# sourceMappingURL=02-video.4c5d09f3.js.map
