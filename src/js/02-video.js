d7c3712edc07f43c46572a60c03922aaf2df3381
Roman <romeenice@gmail.com>
2022-12-30 19:46:05 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,21 @@
-<iframe src="https://player.vimeo.com/video/76979871?h=8272103f6e" width="640" height="360" frameborder="0" allowfullscreen allow="autoplay; encrypted-media"></iframe>
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
-<script src="https://player.vimeo.com/api/player.js"></script>
-<script>
-    const iframe = document.querySelector('iframe');
-    const player = new Vimeo.Player(iframe);
+player.on('play', function () {
+  console.log('played the video!');
+});
 
-    player.on('play', function() {
-        console.log('played the video!');
-    });
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-        });
-</script>
+const onPlay = function (data) {
+  // data is an object containing properties specific to that event
+};
+player.on('play', onPlay);
+
+player.on('eventName', function (data) {
+  duration: 61.857;
+  percent: 0;
+  seconds: 0;
+});
