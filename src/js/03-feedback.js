89d6cfb9c5a1395b1ca18af01f7f4ee8204104e0
Roman <romeenice@gmail.com>
2022-12-30 21:55:38 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,6 +2,8 @@ import throttle from 'lodash.throttle';
 
 const STORAGE_KAY = 'feedback-form-state';
 
+const formData = {};
+
 const refs = {
   form: document.querySelector('.feedback-form'),
   textarea: document.querySelector('.feedback-form textarea'),
@@ -9,6 +11,10 @@ const refs = {
 
 refs.form.addEventListener('submit', onFormSubmit);
 refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
+
+refs.form.addEventListener('input', evt => {
+  formData[evt.target.name] = evt.target.value;
+});
 
 populateTextarea();
 
@@ -23,12 +29,12 @@ function onFormSubmit(evt) {
 
 function onTextareaInput(evt) {
   const message = evt.target.value;
-  localStorage.setItem(STORAGE_KAY, message);
+  localStorage.setItem(STORAGE_KAY, JSON.stringify(formData));
 }
 
 function populateTextarea() {
   const savedMessage = localStorage.getItem(STORAGE_KAY);
-  if (savedMessage) {
+  if (JSON.parse(savedMessage)) {
     refs.textarea.value = savedMessage;
   }
 }
