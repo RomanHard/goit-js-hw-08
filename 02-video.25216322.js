5791e0332edc2216d9fd38c47a735f0d0e766abc
RomanHard <86237018+RomanHard@users.noreply.github.com>
2022-12-30 17:59:34 +0000

    Deploying to gh-pages from @ RomanHard/goit-js-hw-08@33b6882da40f793c9d4ce1d3206be31005a58355 🚀

--- 02-video.25216322.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("iframe"),t=new Vimeo.Player(e);t.on("play",(function(){console.log("played the video!")})),t.getVideoTitle().then((function(e){localStorage.setItem("videoplayer-current-time",JSON.stringify(e.seconds))})),t.on("timeupdate",throttle(getCurrentPlayTime,1e3));localStorage.getItem("videoplayer-current-time")&&t.setCurrentTime(JSON.parse(localStorage.getItem("videoplayer-current-time"))).then((function(e){console.log(`Last time you stopped video on ${second} seconds`)}));
-//# sourceMappingURL=02-video.25216322.js.map
